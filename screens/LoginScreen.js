6fd8d3f787c88807071522c849496b6a81faff3b
UtkuHazarGel <155973196+UtkuHazarGel@users.noreply.github.com>
2024-01-09 15:44:02 +0300

    Sayfanın bileşenleri eklendi

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, Touchable, TouchableOpacity} from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <KeyboardAvoidingView>
+      <View>
+        <TextInput placeholder='Email'/>
+        <TextInput placeholder='Şifre'/>
+
+      
+      </View>
+      <View>
+      <TouchableOpacity>
+          <Text> Giriş yap</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text> Kayıt ol</Text>
+        </TouchableOpacity>
+      </View>
+     
+    </KeyboardAvoidingView>
   )
 }
 
